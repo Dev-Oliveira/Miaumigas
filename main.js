191fc26f8667cfb5138380fbb81c2c81691688fa
Hellom Oliveira <hellom-mendes@hotmail.com>
2021-07-01 15:02:13 +0100

    desenvolvendo 1.5

--- main.js
+++ main.js
@@ -18,3 +18,72 @@ for(const link of links) {
         nav.classList.remove('show')
     })
 }
+
+/* Mudar o header da pagina quando der scroll */
+
+
+
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+
+
+    if(window.scrollY >= navHeight){
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }   
+
+}
+
+
+
+/* Testimonials carousel swiper */
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el:'.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+
+  });
+
+  /*  ScrollReveal: Mostrar elementos quando der scroll na pagina */
+
+  const scrollReveal = ScrollReveal({
+        origin: 'top',
+        distance: '30px',
+        duration: 700,
+        reset: true
+  })
+
+  scrollReveal.reveal(`
+    
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+  `, { interval: 100})
+
+
+  /* botão voltar para o topo */
+
+  
+  function backToTop(){
+    const backToTopButton = document.querySelector('.back-to-top')
+    if(window.scrollY>=560){
+        backToTopButton.classList.add('show')
+      } else {
+        backToTopButton.classList.remove('show')
+      }
+    
+  }
+  /* when Scroll */
+    window.addEventListener('scroll', function(){
+    changeHeaderWhenScroll()    
+    backToTop()
+    })
